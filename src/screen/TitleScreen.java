f091f493116a3278de82d6b28b1409fdfbc943ce
jo-gunhee <no1gun2@hanyang.ac.kr>
2021-12-08 21:26:45 +0900

    21.12.08

--- src/screen/TitleScreen.java
+++ src/screen/TitleScreen.java
@@ -15,7 +15,7 @@ public class TitleScreen extends Screen {
 
 	/** Milliseconds between changes in user selection. */
 	private static final int SELECTION_TIME = 200;
-	
+
 	/** Time between changes in user selection. */
 	private Cooldown selectionCooldown;
 
@@ -23,11 +23,11 @@ public class TitleScreen extends Screen {
 	 * Constructor, establishes the properties of the screen.
 	 * 
 	 * @param width
-	 *            Screen width.
+	 *               Screen width.
 	 * @param height
-	 *            Screen height.
+	 *               Screen height.
 	 * @param fps
-	 *            Frames per second, frame rate at which the game is run.
+	 *               Frames per second, frame rate at which the game is run.
 	 */
 	public TitleScreen(final int width, final int height, final int fps) {
 		super(width, height, fps);
